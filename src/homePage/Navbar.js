ce769188d0ce753ca1d5789d1971a1584d378e15
saifkhan29 <79800224+saifkhan29@users.noreply.github.com>
2021-07-19 22:55:01 +0530

    Update Navbar.js

--- src/homePage/Navbar.js
+++ src/homePage/Navbar.js
@@ -32,20 +32,20 @@ export default function Navbar() {
     return (
         <div>
             <nav>
-      <div class="logo">
+      <div className="logo">
         <h4>HR Tech | Insurance Tech</h4>
       </div>
-      <ul class="nav-links">
+      <ul className="nav-links">
         <li><a href="#">Home</a></li>
         <li><a href="#about">About</a></li>
         <li><a href="#">Projects</a></li>
         <li><a href="#">Clients</a></li>
         <li><a href="#">Contact</a></li>
       </ul>
-      <div class="burger" onClick={navSlide}>
-        <div class="line1"></div>
-        <div class="line2"></div>
-        <div class="line3"></div>
+      <div className="burger" onClick={navSlide}>
+        <div className="line1"></div>
+        <div className="line2"></div>
+        <div className="line3"></div>
       </div>
     </nav>
             
