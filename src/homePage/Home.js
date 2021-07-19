0e65a179631530b48de776c34b9d5b8aeeda3aed
saifkhan29 <79800224+saifkhan29@users.noreply.github.com>
2021-07-19 22:55:34 +0530

    Update Home.js

--- src/homePage/Home.js
+++ src/homePage/Home.js
@@ -5,10 +5,10 @@ export default function home() {
     return (
         <div>
             <section>
-      <div class="container1">
-        <div class="side1">
-          <div class="side1sub1">
-            <p class="intro">
+      <div className="container1">
+        <div className="side1">
+          <div className="side1sub1">
+            <p className="intro">
               We are a one stop <br />
               solution for all your <br />
               problems related to Loan, <br />
@@ -16,23 +16,23 @@ export default function home() {
               Insurance Health Benefits etc.
             </p>
           </div>
-          <div class="side1sub2">
-            <button class="btn1">More</button>
-            <button class="btn2">Contact</button>
+          <div className="side1sub2">
+            <button className="btn1">More</button>
+            <button className="btn2">Contact</button>
           </div>
         </div>
 
-        <div class="side2">
-          <div class="side2sub1">
+        <div className="side2">
+          <div className="side2sub1">
             <img src="https://user-images.githubusercontent.com/79800224/126149004-a6d0ed20-7aa6-47fd-8a29-5493af764e7c.png" alt="" />
           </div>
-          <div class="side2sub2">
-            <div class="banner">
-              <h1 class="banner-heading">
-                <span class="heading-1">Payroll</span>
-                <span class="heading-2">HR</span>
-                <span class="heading-3">Hiring</span>
-                <span class="heading-4">Loan</span>
+          <div className="side2sub2">
+            <div className="banner">
+              <h1 className="banner-heading">
+                <span className="heading-1">Payroll</span>
+                <span className="heading-2">HR</span>
+                <span className="heading-3">Hiring</span>
+                <span className="heading-4">Loan</span>
               </h1>
             </div>
           </div>
