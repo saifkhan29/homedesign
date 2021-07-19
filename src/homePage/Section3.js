eabf99880c4f2ea6963b243f6a6e86c82feec871
saifkhan29 <79800224+saifkhan29@users.noreply.github.com>
2021-07-19 22:56:46 +0530

    Update Section3.js

--- src/homePage/Section3.js
+++ src/homePage/Section3.js
@@ -5,12 +5,12 @@ export default function Section3() {
     return (
         <div>
             <section id="section3">
-      <div class="container3">
-        <div class="sec3cont1">
-          <div class="sec3side1">
+      <div className="container3">
+        <div className="sec3cont1">
+          <div className="sec3side1">
             <img src="https://user-images.githubusercontent.com/79800224/126148986-6df2ddc6-b156-4a01-9923-2ee100c9b2c9.png" alt="" />
           </div>
-          <div class="sec3side2">
+          <div className="sec3side2">
             <h2>
               A complete HR <br />
               platform <br />
